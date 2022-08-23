7108e7d32c40e5e66ca620b154f4203f150ed253
NathanAugustinov-Endava <nathan.augustinov@endava.com>
2022-08-23 17:11:53 +0300

    Redesigned your books page and created add book for rent form

--- frontend/src/components/your-books-page/YourBooksPage.js
+++ frontend/src/components/your-books-page/YourBooksPage.js
@@ -39,13 +39,47 @@ const YourBooksPage = () => {
 
     return (
         <div className="bookslist">
-            <button type="submit" className="btn">Add new book</button>
-             <div className="wrapper">
-                {yourBooks.map(book => (
-                    <div key={book.bookId} className="flex_child">
-                        <Book id={book.bookId}/>
+            <div className="div_container">
+                {/* <button type="submit" className="btn">Add new book</button> */}
+                <div className="flex_child">
+                    <div className="register_form">
+                        <p className="addBookMessage">Add a new book for rent</p>
+                        <div className="form_body">
+                            <div className="title">
+                                <label className="form_label">Title </label>
+                                <input className="form_input" type="text" id="title"/>
+                            </div>
+                            <div className="author">
+                                <label className="form_label">Author </label>
+                                <input className="form_input" type="text" id="author" />
+                            </div>
+                            <div className="description">
+                                <label className="form_label">Description </label>
+                                <input className="form_input" type="email" id="description" />
+                            </div>
+                            <div className="published_date">
+                                <label className="form_label">Published date </label>
+                                <input className="form_input" type="text" id="published_date" />
+                            </div>
+                            <div className="image">
+                                <label className="form_label">Image </label>
+                                <input className="form_input" type="file"  id="image"/>
+                            </div>
+                        </div>
+                        <div className="footer">
+                            <button type="submit" className="btn">Add book</button>
+                        </div>
                     </div>
-                ))}
+                </div> 
+                <div className="flex_child">
+                    <div className="wrapper">
+                        {yourBooks.map(book => (
+                            <div key={book.bookId} className="flex_child">
+                                <Book id={book.bookId}/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
